ee3fc5101428f22a02db83df42a66e7e89588b9a
lq0000 <root@lo.calho.st>
2016-12-21 07:55:07 -0700

    add existing code

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+
+#include "MedianFilter.h"
+
+int main(int argc, char **argv)
+{
+
+    MedianFilter<int, 7> f;
+    
+    for(int i = 0; i < 1000; i++){
+        int s = rand() % 100;
+        f.addSample(s);
+        if(f.isReady()){
+            std::cout << f.getMedian() << std::endl;
+        }
+    }
+    
+    return 0;
+
+}
